14b13e8ee92be289c20e96ad19b4be91909d3176
Jack Steel <jacksteel@jacksteel.co.uk>
2016-11-23 15:09:29 +0000

    added vowel count calculation

--- TextAnalysis/Sentence.cs
+++ TextAnalysis/Sentence.cs
@@ -10,7 +10,6 @@ namespace TextAnalysis
     {
         private string sentence = "";
         private int wordCount = 0;
-        private int characterCount = 0;
         private int vowelCount = 0;
         private int consonantCount = 0;
         private int uppercaseCount = 0;
@@ -22,6 +21,9 @@ namespace TextAnalysis
             this.sentence = sentenceContent;
 
             //calculate word count
+            calculateWordCount();
+
+            //calculate vowel count
         }
 
 
@@ -37,6 +39,21 @@ namespace TextAnalysis
             }
             words++;
             this.wordCount = words;
+        }
+
+        private void calculateVowelCount()
+        {
+            int vowels = 0;
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                string currentChar = sentence[i].ToString().ToLower();
+                if (currentChar == "a" || currentChar == "e" || currentChar == "i" || currentChar == "o" || currentChar == "u")
+                {
+                    vowels++;
+                }
+            }
+            vowels++;
+            this.vowelCount = vowels;
         }
 
     }
